1347c1ab95572c1944e150a3b057a2825b5e1639
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2022-03-07 12:42:59 +0700

    add goerli RPC
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- api/goerli.js
+++ api/goerli.js
@@ -1,5 +1,5 @@
 const tx = require("../rlp-tx-construction")
-const txConstructor = new tx("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161")
+const txConstructor = new tx(process.env.GOERLI_RPC)
 
 module.exports = async (req, res) => {
     const { txHash } = req.query
