b7b1d4788127adf72b73cafd27d2f9af1d0ee80f
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2022-03-07 12:38:02 +0700

    change to using async function
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- api/goerli.js
+++ api/goerli.js
@@ -1,7 +1,7 @@
 const tx = require("../rlp-tx-construction")
 const txConstructor = new tx("https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161")
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
     const { txHash } = req.query
     const rawTransaction = await txConstructor.getRawTransaction(txHash)
     res.json({
