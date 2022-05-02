481bea00c34ced4dfd23acd4a4301f44a2b59207
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2022-05-03 05:23:44 +0700

    change respond type
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- api/ens.js
+++ api/ens.js
@@ -4,7 +4,5 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC);
 module.exports = async (req, res) => {
     const { address } = req.query
     const ensDomain = await provider.lookupAddress(address)
-    res.json({
-        ensDomain
-    })
+    res.end(ensDomain)
 }
